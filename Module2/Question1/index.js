a6ab2c026f85b2d6137fc5ee2a941cce8df80da9
BavurupudiNavya2005 <bavurupudinavyasri@gmail.com>
2025-11-23 19:55:08 +0530

    QA1

--- Module2/Question1/index.js
+++ Module2/Question1/index.js
@@ -1,6 +1,48 @@
+//Q1
+
 let arr1 = [10, 20, 30];
 let arr2 = [40, 50];
 
 let combined = [...arr1, ...arr2]
 
 console.log(combined);
+
+
+//Q2
+
+let person = { name: "Venu", age: 25 };
+let extra = { city: "Bengaluru" };
+
+let profile = { ...person, ...extra}
+
+console.log(profile);
+
+
+//Q3
+
+function sumAll(...numbers){
+    return numbers
+}
+
+console.log(sumAll(1, 2, 3, 4))
+
+
+//Q4
+
+let numbers = [10, 20, 30, 40, 50];
+
+let [a, ...remaining] = numbers;
+console.log(numbers);
+
+
+//Q5
+
+let user = {
+  name: "Alice",
+  address: {
+    city: "Bengaluru",
+    pin: 560001,
+    geo: { lat: 11.22, lng: 77.33 }
+  }
+};
+
